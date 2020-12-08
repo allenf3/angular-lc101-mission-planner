1ab22b9e48ecf220927e7d2babe9a7e8a9fa41e4
Fred Allen <allenf3@msn.com>
2020-12-08 14:15:35 -0600

    Added formatting

--- mission-planning-dashboard/src/app/experiments/experiments.component.ts
+++ mission-planning-dashboard/src/app/experiments/experiments.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experiments',
   templateUrl: './experiments.component.html',
-  styleUrls: ['./experiments.component.css']
+  styleUrls: ['./experiments.component.css', '../app.component.css']
 })
 export class ExperimentsComponent implements OnInit {
 
